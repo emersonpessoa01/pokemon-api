2ba35bed21c70e9a425f75145ab81fa06bf25916
emersonpessoa01 <eps011108@gmail.com>
2020-11-19 15:44:35 -0300

    method implemented  pokemonRouter

--- controller/pokemonController.js
+++ controller/pokemonController.js
@@ -1,26 +1,49 @@
-import { db } from '../models/index.js';
+import { db } from "../models/index.js";
 
+const Pokemon = db.pokemon;
 const create = async (req, res) => {
+  const pokemon = new Pokemon({
+    Pokemon: req.body.Pokemon,
+    GIF: req.body.GIF,
+    HP: req.body.HP,
+    Attack: req.body.Attack,
+    Defense: req.body.Defense,
+    Speed: req.body.Speed,
+    Description: req.body.Description,
+  });
+
   try {
+    await pokemon.save();
+    res.send({
+      message: "Pokemon inserido com sucesso",
+    });
   } catch (error) {
     res
       .status(500)
-      .send({ message: error.message || 'Algum erro ocorreu ao salvar' });
+      .send({ message: error.message || "Algum erro ocorreu ao salvar" });
   }
 };
 
 const findAll = async (req, res) => {
-  const name = req.query.name;
+  const name = req.query.Pokemon;
 
   var condition = name
-    ? { name: { $regex: new RegExp(name), $options: 'i' } }
+    ? { name: { $regex: new RegExp(name), $options: "i" } }
     : {};
 
   try {
+    const data = await Pokemon.find(condition);
+    if (data.length < 1) {
+      res.status(404).send({
+        message: "Pokemon nao encontrado",
+      });
+    } else {
+      res.send(data);
+    }
   } catch (error) {
     res
       .status(500)
-      .send({ message: error.message || 'Erro ao listar todos os documentos' });
+      .send({ message: error.message || "Erro ao listar todos os documentos" });
   }
 };
 
@@ -28,23 +51,42 @@ const findOne = async (req, res) => {
   const id = req.params.id;
 
   try {
+    const data = await Pokemon.findById({ _id: id });
+    if (data.length < 1) {
+      res.status(404).send({
+        message: `Pokemon id:${id} nao encontrado`,
+      });
+    } else {
+      res.send(data);
+    }
   } catch (error) {
-    res.status(500).send({ message: 'Erro ao buscar o Documento id: ' + id });
+    res.status(500).send({ message: "Erro ao buscar o Documento id: " + id });
   }
 };
 
 const update = async (req, res) => {
   if (!req.body) {
     return res.status(400).send({
-      message: 'Dados para atualizacao vazio',
+      message: "Dados para atualizacao vazio",
     });
   }
 
   const id = req.params.id;
 
   try {
+    const data = await Pokemon.findByIdUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+
+    if (data.length < 1) {
+      res.status(404).send({
+        message: "Pokemon nao encontrado para atualizacao",
+      });
+    } else {
+      res.send(data);
+    }
   } catch (error) {
-    res.status(500).send({ message: 'Erro ao atualizar o Pokemon id: ' + id });
+    res.status(500).send({ message: "Erro ao atualizar o Pokemon id: " + id });
   }
 };
 
@@ -52,19 +94,35 @@ const remove = async (req, res) => {
   const id = req.params.id;
 
   try {
+    const data = await Pokemon.findByIdRemove({ _id: id });
+    if (data.length < 1) {
+      res.send({
+        message: "Pokemon excluido com sucesso",
+      });
+    } else {
+      res.send(data);
+    }
   } catch (error) {
     res
       .status(500)
-      .send({ message: 'Nao foi possivel deletar o Pokemon id: ' + id });
+      .send({ message: "Nao foi possivel deletar o Pokemon id: " + id });
   }
 };
 
 const removeAll = async (req, res) => {
-  const id = req.params.id;
-
   try {
+    const data = await Pokemon.deleteMany();
+    if (data.length < 1) {
+      res.status(404).send({
+        message: "Nenhum Pokemon encontrado para exclusao",
+      });
+    } else {
+      res.send({
+        message: "Pokemon excluidos com sucesso",
+      });
+    }
   } catch (error) {
-    res.status(500).send({ message: 'Erro ao excluir todos os Pokemons' });
+    res.status(500).send({ message: "Erro ao excluir todos os Pokemons" });
   }
 };
 
